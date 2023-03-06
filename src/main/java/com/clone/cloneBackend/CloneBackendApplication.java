a4bf0b20951f5bee821d8d82fa3113c21ba0c228
temiOlamide <owolabitemiolamide@gmail.com>
2023-03-06 05:30:30 +0100

    adjusted port changes

--- src/main/java/com/clone/cloneBackend/CloneBackendApplication.java
+++ src/main/java/com/clone/cloneBackend/CloneBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CloneBackendApplication {
-	public String PORT = System.getenv("PORT");
+//	public String PORT = System.getenv("PORT");
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloneBackendApplication.class, args);
